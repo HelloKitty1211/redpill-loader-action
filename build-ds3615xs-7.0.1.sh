e2479f5b1dfb5962e3775d6485a5f929692f60fc
HelloKitty1211 <55379276+HelloKitty1211@users.noreply.github.com>
2022-01-05 16:37:33 +0800

    Update build-ds3615xs-7.0.1.sh

--- build-ds3615xs-7.0.1.sh
+++ build-ds3615xs-7.0.1.sh
@@ -8,15 +8,19 @@ mkdir DS3615xs-7.0.1
 mkdir output
 cd DS3615xs-7.0.1
 
-sudo git clone https://github.com/tossp/redpill-tool-chain.git
+# sudo git clone https://github.com/tossp/redpill-tool-chain.git
+sudo git clone https://github.com/guigeng/redpill-helper.git
 ls -l
 
-cd redpill-tool-chain
-sudo cp -a sample_user_config.json bromolow_user_config.json
+cd redpill-helper
+# sudo cp -a sample_user_config.json bromolow_user_config.json
 ls -l
-sudo ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8168/rpext-index.json
-sudo ./redpill_tool_chain.sh build bromolow-7.0.1-42218
-sudo ./redpill_tool_chain.sh auto bromolow-7.0.1-42218
+# sudo ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8168/rpext-index.json
+# sudo ./redpill_tool_chain.sh build bromolow-7.0.1-42218
+# sudo ./redpill_tool_chain.sh auto bromolow-7.0.1-42218
+sudo ./rp-helper.sh ext bromolow-7.0.1-42218 add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8168/rpext-index.json
+sudo ./rp-helper.sh build bromolow-7.0.1-42218
+sudo ./rp-helper.sh auto bromolow-7.0.1-42218
 ls images -l
 sudo mv images/redpill-DS3615xs_7.0.1-42218*.img ${root}/output/
 cd ${root}
