ca6c4313f188d58fdf8ec39f16dbea39c7933f59
HelloKitty1211 <55379276+HelloKitty1211@users.noreply.github.com>
2022-01-05 15:43:41 +0800

    Update build-ds3615xs-7.0.1.sh

--- build-ds3615xs-7.0.1.sh
+++ build-ds3615xs-7.0.1.sh
@@ -14,9 +14,9 @@ ls -l
 cd redpill-tool-chain
 sudo cp -a sample_user_config.json bromolow_user_config.json
 ls -l
-sudo /bin/bash redpill-tool-chain add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8168/rpext-index.json
-sudo /bin/bash redpill_tool_chain build bromolow-7.0.1-42218
-sudo /bin/bash redpill_tool_chain auto bromolow-7.0.1-42218
+sudo ./redpill_tool_chain.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8168/rpext-index.json
+sudo ./redpill_tool_chain.sh build bromolow-7.0.1-42218
+sudo ./redpill_tool_chain.sh auto bromolow-7.0.1-42218
 
 mv images/redpill-DS3615xs_7.0.1-42218*.img ${root}/output/
 cd ${root}
